8ba28b6732c99a36d788eec1c17f5f4de6b5314b
Tim Heap <tim@ionata.com.au>
2012-11-12 14:30:18 +1100

    Fix up setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 try:
-	from setuptools import setup, find_packages
+    from setuptools import setup, find_packages
 except ImportError:
-	from ez_setup import use_setuptools
-	use_setuptools()
-	from setuptools import setup, find_packages
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 setup(
     name='django-featured-item',
@@ -14,8 +14,8 @@ setup(
     author='Tim Heap',
     author_email='heap.tim@gmail.com',
     url='https://bitbucket.org/tim_heap/django-featured-item',
-    packages=['featured_item',],
-    install_requires = ['Django>=1.4'],
+    packages=find_packages(),
+    install_requires=['Django>=1.4'],
     package_data={},
     classifiers=[
         'Environment :: Web Environment',
@@ -25,4 +25,3 @@ setup(
         'Framework :: Django',
     ],
 )
-
